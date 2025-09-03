c6911c901812b2cce53b879560ceb4c7db8a3293
RamonVBN <ramon@PC-RAMON.>
2025-09-02 23:13:34 -0300

    ...

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -47,8 +47,8 @@ export class SessionController {
 
         response.cookie("access_token", token, {
             httpOnly: true,    // não acessível via JS -> mais seguro
-            secure: env.NODE_ENV === 'production',      // só HTTPS (em dev, false)
-            sameSite: env.NODE_ENV === 'production' ? 'none': 'lax',   // previne CSRF na maioria dos casos
+            secure: true,      // só HTTPS (em dev, false)
+            sameSite: 'none',   // previne CSRF na maioria dos casos
             maxAge: 1000 * 60 * 120, // 15 min
             path: "/",         // válido em todo o site
 
